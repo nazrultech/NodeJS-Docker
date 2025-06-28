7a5c7b4d41b3beb111e49c56865835c1af105fbc
Nazrul <nihalahmed@gmail.com>
2025-06-28 22:28:11 +0530

    updated folder structure

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`
@@ -25,6 +24,10 @@ app.get('/', (req, res) => {
   `);
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
