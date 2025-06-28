2fc4eada1ddbd834557c4e555f36d25224c3eae6
Nazrul <nihalahmed@gmail.com>
2025-06-29 00:13:05 +0530

    updated

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const port = 3000;
+
+let userGoal = 'Teach Docker!';
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js inside Docker!');
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h2>Docker commands recap</h2>
+          <h3>${userGoal}</h3>
+        </section>
+        <form action="/store-goal" method="POST">
+          <div class="form-control">
+            <label>Docker Commands</label>
+            <input type="text" name="goal">
+          </div>
+          <button>Commands</button>
+        </form>
+      </body>
+    </html>
+  `);
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`App listening at http://0.0.0.0:${port}`);
+app.post('/store-goal', (req, res) => {
+  const enteredGoal = req.body.goal;
+  console.log(enteredGoal);
+  userGoal = enteredGoal;
+  res.redirect('/');
 });
+
+app.listen(3000);
